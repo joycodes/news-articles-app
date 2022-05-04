dd1edcd1744981693978a012211567ff2fe4e2c3
joycodes <jnguttu@gmail.com>
2022-05-04 13:31:04 +0300

    edit tests file

--- tests/test_keyword.py
+++ tests/test_keyword.py
@@ -11,8 +11,8 @@ class KeywordTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_keyword = Keyword('Stan Schroeder', 'Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.',
-                                   'https://abcnews.go.com', '2020-05-16T00:34:00Z', 'https://mashable.com/article/bitcoin-halving-2020/')
+        self.new_keyword = Keyword('Jeff Grubb', 'Are Square JRPGs endangered on Switch? | Last of the Nintendogs',
+                                    'https://abcnews.com', '2022-05-04T01:03:30Z', 'https://venturebeat.com/2022/05/03/are-square-jrpgs-endangered-on-switch-last-of-the-nintendogs/')
 
     def test_instance(self):
         '''
@@ -24,8 +24,8 @@ class KeywordTest(unittest.TestCase):
         '''
         Test initialization test case to test if the object is initialized properly
         '''
-        self.assertEqual(self.new_keyword.author, 'Stan Schroeder')
-        self.assertEqual(self.new_keyword.imageurl, 'https://abcnews.go.com')
+        self.assertEqual(self.new_keyword.author, 'Jeff Grubb')
+        self.assertEqual(self.new_keyword.imageurl, 'https://abcnews.com')
 
 
 if __name__ == '__main__':
