b18f61411baab7824ab6fbf4588caaeac935fb81
joycodes <jnguttu@gmail.com>
2022-05-04 07:08:24 +0300

    changes in views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,30 +1,33 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, request, redirect, url_for
 from . import main
-from ..request import get_sources, get_top_articles,get_keyword, get_breaking_news
+from ..request import get_sources, get_top_articles, get_keyword, get_breaking_news
 
 # Views
+
+
 @main.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
-    #Getting sources
+    # Getting sources
     sources = get_sources()
     breaking_news = get_breaking_news()
-    title = 'The Daily Telegram'
+    title = 'News Articles App'
     get_keyword = request.args.get('keyword_query')
 
     if get_keyword:
         return redirect(url_for('main.keyword', keyword_name=get_keyword))
     else:
-        return render_template('index.html', title = title, sources=sources, breaking_news=breaking_news)
+        return render_template('index.html', title=title, sources=sources, breaking_news=breaking_news)
+
 
 @main.route('/source/<sourcesId>')
 def articles(sourcesId):
     '''
     View function that displays top stories from a particular source
     '''
-    articles= get_top_articles(sourcesId)
+    articles = get_top_articles(sourcesId)
     title = f"{sourcesId}"
     header = sourcesId.upper()
     get_keyword = request.args.get('keyword_query')
@@ -33,6 +36,7 @@ def articles(sourcesId):
         return redirect(url_for('main.keyword', keyword_name=get_keyword))
     else:
         return render_template('articles.html', title=title, header=header, article=articles)
+
 
 @main.route('/search/<keyword_name>')
 def keyword(keyword_name):
@@ -45,4 +49,4 @@ def keyword(keyword_name):
     title = f'{keyword_name.title()}'
     display_keyword = keyword_name.upper()
 
-    return render_template('search.html', keyword = searched_keyword, title = title, display_keyword=display_keyword )
+    return render_template('search.html', keyword=searched_keyword, title=title, display_keyword=display_keyword)
