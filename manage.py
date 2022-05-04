302600e67ae3554e159f930b5b35d2bab43f5022
joycodes <jnguttu@gmail.com>
2022-05-04 08:25:59 +0300

    commit changes

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
-from app import create_app
+
 from flask_script import Manager, Server
+
+from app import create_app
+
 
 # Creating app instance
 app = create_app('development')
-
 manager = Manager(app)
 manager.add_command('server', Server)
 
