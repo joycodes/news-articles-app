e689e183f418abbf066661fb361187ab648757d6
joycodes <jnguttu@gmail.com>
2022-05-05 22:01:57 +0300

    deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -7,13 +7,13 @@ from ..requests import get_news_sources, get_news_by_source, get_all_news
 def index():
     all_sources = get_news_sources()
     news_items = get_all_news()
-    title = "Global news"
+    title = "News Today"
     return render_template("index.html", sources=all_sources, title=title, others=news_items)
 
 
 @main.route('/source/<source>')
 def news_healines(source):
-    title = "Global news"
+    title = "News Today"
     news_articles = get_news_by_source(source)
     return render_template('news_article.html',title=title, headlines=news_articles)
 
